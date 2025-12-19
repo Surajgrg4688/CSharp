b57f0a297e3067455f7ddf2b6f66fade3107bc44
Suraj Gurung <surajgurung@Surajs-MacBook-Air.local>
2025-12-19 22:51:13 +0930

    Initial proj1: find greatest among three numbers

---
+++ CSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.XPath;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        //write code to find greatest number between 3 numbers. ask user to input 3 numbers
+        Console.WriteLine("Enter first number:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter third number:");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+        int greatest;
+        if (num1 >= num2 && num1 >= num3)
+        {
+            greatest = num1;
+        }
+        else if (num2 >= num1 && num2 >= num3)
+        {
+            greatest = num2;
+        }
+        else
+        {
+            greatest = num3;
+        }
+        Console.WriteLine("The greatest number is: " + greatest);
+
+    }
+
+
+    
+
+}
